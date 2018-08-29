7f63cfa1c1ba9791eac7d625201dde1eb3d4772d
Kfir Zuberi <kfir.z@walkme.com>
2018-08-29 13:23:32 +0300

    first commit

---
+++ MasterScripter/MasterScripter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MasterScripter.MvcApplication" Language="C#" %>
---
+++ MasterScripter/MasterScripter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MasterScripter.Startup))]
+namespace MasterScripter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
