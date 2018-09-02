cd6bf14d9be01868c1ea3253b3452e5d5b74c8ef
Kfir Zuberi <kfir.z@walkme.com>
2018-09-02 18:07:13 +0300

    Add files

---
+++ MasterScripter/MasterScripter/Controllers/ManagementController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MasterScripter.Models;
+
+namespace MasterScripter.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Management
+        public ActionResult Index()
+        {
+            var managementViewModel = new ManagementViewModel()
+            {
+                Companies = DAL.Utils.FakeData.GetCompanies(),
+                Reasons = DAL.Utils.FakeData.GetReasons()
+            };
+
+            return View(managementViewModel);
+        }
+    }
+}
---
+++ MasterScripter/MasterScripter/Models/ManagementViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MasterScripter.DAL.Models;
+
+namespace MasterScripter.Models
+{
+    public class ManagementViewModel
+    {
+        public ICollection<Company> Companies { get; set; }
+        public ICollection<Reason> Reasons { get; set; }
+    }
+}
