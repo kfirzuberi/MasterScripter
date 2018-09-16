32dc78da05d3a53738ed4933836797ea70be7263
Kfir Zuberi <kfir.z@walkme.com>
2018-09-16 22:12:51 +0300

    Add video and Analytics

---
+++ MasterScripter/MasterScripter/Controllers/AnalyticsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MasterScripter.DAL.Models;
+using MasterScripter.Models;
+
+namespace MasterScripter.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        private MasterScripterContext db = new MasterScripterContext();
+
+        // GET: Analytics
+        public ActionResult Index()
+        {
+            Dictionary<Status, int> totalStatuses = db.Executions.ToList().GroupBy(execution => execution.Status)
+                .ToList().ToDictionary(input => input.Key, grouping => grouping.Count());
+
+            Dictionary<Reason, int> totalReasons = db.Executions.ToList().GroupBy(execution => execution.Reason)
+                .ToList().ToDictionary(input => input.Key, grouping => grouping.Count());
+
+            Dictionary<Country, int> totalCountries = db.Machines.ToList().GroupBy(execution => execution.Country)
+                .ToList().ToDictionary(input => input.Key, grouping => grouping.Count());
+
+
+            var myData = from log in db.Executions
+                group log by EntityFunctions.TruncateTime(log.CreationDate) into g
+                orderby g.Key
+                select new { CreateTime = g.Key, Count = g.Count() };
+
+            Dictionary<DateTime?, int> rr = myData.ToList().ToDictionary(i=>i.CreateTime, i=>i.Count);
+
+            AnalyticsViewModel analyticsViewModel = new AnalyticsViewModel()
+            {
+                TotalStatuses =  totalStatuses,
+                TotalCountries = totalCountries,
+                TotalReasons = totalReasons
+            };
+
+            return PartialView(analyticsViewModel);
+        }
+    }
+}
---
+++ MasterScripter/MasterScripter/Models/AnalyticsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MasterScripter.DAL.Models;
+
+namespace MasterScripter.Models
+{
+
+    public class AnalyticsViewModel
+    {
+        public Dictionary<Status, int> TotalStatuses { get; set; }
+        public Dictionary<Reason, int> TotalReasons { get; set; }
+        public Dictionary<Country, int> TotalCountries { get; set; }
+        public Dictionary<string, int> TotalMachines { get; set; }
+        public Dictionary<DateTime, int> TotalExecutionsPerDay { get; set; }
+        public Dictionary<DateTime, TimeSpan> AvgExecutionsDuration { get; set; }
+    }
+}
